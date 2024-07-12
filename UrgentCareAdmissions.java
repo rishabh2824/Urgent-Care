3337a7f334f18be27db47bed4b149978cd3611b5
Rishabh Jain <85334960+rishabh2824@users.noreply.github.com>
2024-07-12 12:52:21 +0530

    Update UrgentCareAdmissions.java

--- UrgentCareAdmissions.java
+++ UrgentCareAdmissions.java
@@ -1,6 +1,6 @@
 //////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
 //
-// Title:    Urgent Care Admissions
+// Title:    ExceptionalCareAdmissions.java
 // Course:   CS 300 Spring 2023
 //
 // Author:   Rishabh Jain
@@ -9,212 +9,306 @@
 //
 ///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
 //
-// Persons:         NONE
-// Online Sources:  NONE
+// Persons:         None
+// Online Sources:  None
 //
 ///////////////////////////////////////////////////////////////////////////////
-
+//import statements
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 /**
- * This class contains various methods to perform different tasks of the Urgent care program. It
- * performs tasks such as removing records from the start of the list, finding the correct index for
- * adding a new record based on the triagle level, adding the record, finding the longest waiting
- * patient and displaying a summary of the patient data.
+ * An object-oriented iteration of our UrgentCareAdmissions program. Patient records in the
+ * patientsList are now single PatientRecord objects, containing the same data as before but with a
+ * few additional components, namely: age, gender, and a boolean value indicating whether they have
+ * been seen by a doctor. PatientRecords are still maintained as an oversize array.
  */
+public class ExceptionalCareAdmissions {
 
-public class UrgentCareAdmissions {
-
-  public static final int RED = 0;
-  public static final int YELLOW = 1;
-  public static final int GREEN = 2;
+  private PatientRecord[] patientsList; //An oversize array containing the PatientRecords
+  // currently active in this object
+  private int size; //The number of values in the oversize array
 
   /**
-   * A helper method to find the correct index to insert a new patient at the given triage level.
-   * This should be the index AFTER the last index currently occupied by a patient at that level.
-   * <p>
-   * For example, if the list currently contains: [RED, RED, GREEN, GREEN, GREEN, GREEN, null, null,
-   * null, null] with a size of 6, a RED patient would be inserted at index 2, a YELLOW patient
-   * would also be inserted at index 2, and a GREEN patient would be inserted at index 6.
-   * <p>
-   * If the list is FULL, the method should return -1 regardless of the triage value. This method
-   * must not modify patientsList in any way.
+   * Creates a new, empty ExceptionalCareAdmissions object with the given capacity
    *
-   * @param triage       the urgency level of the next patient's issue, RED YELLOW or GREEN
-   * @param patientsList the current, active list of patient records
-   * @param size         the number of patients currently in the list
-   * @return the index at which the patient should be inserted into the list, or -1 if the list is
-   * full
+   * @param capacity the maximum number of patient records this ExceptionalCareAdmissions object can
+   *                 hold
+   * @throws IllegalArgumentException if the provided capacity is less than or equal to zero
    */
-  public static int getAdmissionIndex(int triage, int[][] patientsList, int size) {
-    if (patientsList.length == size) {
-      return -1;
+  public ExceptionalCareAdmissions(int capacity) throws IllegalArgumentException {
+    //Throw exception at invalid capacity
+    if (capacity <= 0) {
+      throw new IllegalArgumentException("Capacity must be greater than 0");
     }
-
-    if (triage == RED) {
-      for (int i = 0; i < size; i++) {
-        if (patientsList[i][2] != RED) {
-          return i;
-        }
-      }
-
-    } else if (triage == YELLOW) {
-      for (int i = 0; i < size; i++) {
-        if (patientsList[i][2] != RED && patientsList[i][2] != YELLOW) {
-          return i;
-        }
-      }
-
-    } else {
-      return size;
-    }
-
-    return 0;
+    //Create a new patient list of the given capacity
+    this.patientsList = new PatientRecord[capacity];
+    this.size = 0;
   }
 
   /**
-   * Adds the patient record, a three-element perfect size array of ints, to the patients list in
-   * the given position. This method must maintain the ordering of the rest of the array, so any
-   * patients in higher index positions must be shifted out of the way.
-   * <p>
-   * If there is no room to add a new patient to the array or the index provided is not a valid
-   * index into the oversize array (for adding, valid indexes are 0 through size), the method should
-   * not modify the patientsList array in any way.
+   * An accessor method to determine if the patientsList is currently full
    *
-   * @param patientRecord a three-element perfect size array of ints, containing the patient's
-   *                      5-digit case ID, their admission order number, and their triage level
-   * @param index         the index at which the patientRecord should be added to patientsList,
-   *                      assumed to correctly follow the requirements of getAdmissionIndex()
-   * @param patientsList  the current, active list of patient records
-   * @param size          the number of patients currently in the list
-   * @return the number of patients in patientsList after this method has finished running
+   * @return true if the patientsList is full, false otherwise
    */
-
-  public static int addPatient(int[] patientRecord, int index, int[][] patientsList, int size) {
-    if (patientsList.length == size) {
-      return size;
+  public boolean isFull() {
+    if (size == patientsList.length) {
+      return true;
     }
-
-    for (int i = size; i > index; i--) {
-      patientsList[i] = patientsList[i - 1];
-    }
-
-    patientsList[index] = patientRecord;
-    size++;
-    return size;
+    return false;
   }
 
   /**
-   * Removes the patient record at index 0 of the patientsList, if there is one, and updates the
-   * rest of the list to maintain the oversize array in its current ordering.
+   * Accesses the current number of patient records in the patientsList
    *
-   * @param patientsList the current, active list of patient records
-   * @param size         the number of patients currently in the list
-   * @return the number of patients in patientsList after this method has finished running
+   * @return the current number of PatientRecord objects in this ExceptionalCareAdmissions object
    */
-  public static int removeNextPatient(int[][] patientsList, int size) {
-    if (size == 0) {
-      return 0;
-    }
-    for (int i = 0; i < size - 1; i++) {
-      patientsList[i] = patientsList[i + 1];
-    }
+  public int size() {
 
-    patientsList[--size] = null;
-    return size;
+    return this.size;
   }
 
   /**
-   * Finds the index of a patient given their caseID number. This method must not modify
-   * patientsList in any way.
+   * Accesses the current number of patient records in this patientsList representing patients who
+   * have already been seen (and could be removed from the list)
    *
-   * @param caseID       the five-digit case number assigned to the patient record to find
-   * @param patientsList the current, active list of patient records
-   * @param size         the number of patients currently in the list
-   * @return the index of the patient record matching the given caseID number, or -1 if the list is
-   * empty or the caseID is not found
+   * @return the current count of patientRecords for which the hasBeenSeen() method returns true
    */
-
-  public static int getPatientIndex(int caseID, int[][] patientsList, int size) {
-    if (size == 0) {
-      return -1;
-    }
-
+  public int getNumberSeenPatients() {
+    int numPatientsSeen = 0;
     for (int i = 0; i < size; i++) {
-      if (patientsList[i][0] == caseID) {
-        return i;
+      if (patientsList[i].hasBeenSeen()) {
+        numPatientsSeen++;
       }
     }
-    return -1;
+    return numPatientsSeen;
   }
 
   /**
-   * Finds the patient who arrived earliest still currently present in the patientsList, and returns
-   * the index of their patient record within the patientsList. The arrival value is strictly
-   * increasing for each new patient, so you will not need to handle the case where two values are
-   * equal.
-   * <p>
-   * That is, for all patient records [ caseID, arrivalOrder, triage ], this method should find and
-   * return the one with the minimum arrivalOrder value.
-   * <p>
-   * This method must not modify patientsList in any way.
+   * As before, this helper method will find the correct index to insert the new patient record into
+   * patientsList, maintaining triage priority order. It uses objects and exceptions rather than
+   * primitive values.
    *
-   * @param patientsList the current, active list of patient records
-   * @param size         the number of patients currently in the list
-   * @return the index of the patient record with the smallest value in their arrival integer, or -1
-   * if the list is empty
+   * @param rec the PatientRecord to be added to the list
+   * @return the correct index of patientsList at which rec should be added
+   * @throws IllegalStateException if the patientsList is full: - with the message
+   *                               "cleanPatientsList()" if the patientsList contains any patients
+   *                               who have been seen, or - with the message "Cannot admit new
+   *                               patients" if there are NO seen patients in the patientsList
    */
-
-  public static int getLongestWaitingPatientIndex(int[][] patientsList, int size) {
-    if (size == 0) {
-      return -1;
+  public int getAdmissionIndex(PatientRecord rec) throws IllegalStateException {
+    //check if patients list is full
+    if (size == patientsList.length) {
+      //check if there are any scene patients
+      if (getNumberSeenPatients() > 0) {
+        //Throw appropriate exception
+        throw new IllegalStateException("cleanPatientsList()");
+      } else {
+        throw new IllegalStateException("Cannot admit new patients");
+      }
     }
-
-    int earliest = Integer.MAX_VALUE;
+    //The index for the record to be inserted
     int index = 0;
-    for (int i = 0; i < size; i++) {
-      if (patientsList[i][1] < earliest) {
-        earliest = patientsList[i][1];
-        index = i;
+    //Check if the triagle is red
+    if (rec.getTriage() == 0) {
+      //Loop through patients list
+      for (int i = 0; i < size; i++) {
+        //The index will be the first non-red triagle patient
+        if (patientsList[i].getTriage() != PatientRecord.RED) {
+          index = i;
+          break;
+        }
       }
+      //check if the triagle is yellow
+    } else if (rec.getTriage() == PatientRecord.YELLOW) {
+      //Loop through patients list
+      for (int i = 0; i < size; i++) {
+        //The index will be the first green patient
+        if (patientsList[i].getTriage() == PatientRecord.GREEN) {
+          index = i;
+          break;
+        }
+      }
+      //If triagle is green then index is the last element (size)
+    } else {
+      index = size;
     }
 
     return index;
   }
 
   /**
-   * Creates a formatted String summary of the current state of the patientsList array, as follows:
-   * <p>
-   * Total number of patients: 5 RED: 1 YELLOW: 3 GREEN: 1
-   * <p>
-   * The first line displays the current size of the array. The next three lines display counts of
-   * patients at each of the three triage levels currently in the patientsList. Any or all of these
-   * numbers may be 0.
-   * <p>
-   * This method must not modify the patientsList array in any way.
+   * Adds the provided PatientRecord at the provided position in the oversize patientsList array.
+   * This method must maintain the ordering of the patientsList as before, and rather than returning
+   * the new size, maintains the size field in this ExceptionalCareAdmissions object appropriately.
    *
-   * @param patientsList the current, active list of patient records
-   * @param size         the number of patients currently in the list
-   * @return a String summarizing the patientsList as shown in this comment
+   * @param rec   the PatientRecord to be added
+   * @param index the index at which the PatientRecord should be added to patientsList, which you
+   *              may assume is the same as the output of getAdmissionIndex()
+   * @throws IllegalStateException    if the patientsList is full: - with the message
+   *                                  "cleanPatientsList()" if the patientsList contains any
+   *                                  patients who have been seen, or - with the message "Cannot
+   *                                  admit new patients" if there are NO seen patients in the
+   *                                  patientsList
+   * @throws IllegalArgumentException with a descriptive error message if the patientsList is NOT
+   *                                  full and the index is not a valid index into the oversize
+   *                                  array
    */
+  public void addPatient(PatientRecord rec, int index)
+      throws IllegalStateException, IllegalArgumentException {
+    //check if patients List is full
+    if (size == patientsList.length) {
+      //check if there are any scene patients
+      if (getNumberSeenPatients() > 0) {
+        //Throw appropriate exception
+        throw new IllegalStateException("cleanPatientsList()");
+      } else {
+        throw new IllegalStateException("Cannot admit new patients");
+      }
+    } else if (index > patientsList.length) {
+      throw new IllegalArgumentException("Invalid index");
+    }
+    //Shift all elements after the index to one place right to free up index
+    for (int i = patientsList.length - 1; i > index; i--) {
+      patientsList[i] = patientsList[i - 1];
+    }
+    //Insert record at the index
+    patientsList[index] = rec;
+    size++;
+  }
 
-  public static String getSummary(int[][] patientsList, int size) {
+  /**
+   * Marks the patient with the given caseID as having been seen. This method will require you to
+   * find the patient with the given caseID within the patientsList before you can modify their
+   * status. This method may only modify one PatientRecord, and may not modify the patientsList
+   * array or its size.
+   *
+   * @param caseID the CASE_NUMBER of the PatientRecord to be marked as having been seen
+   * @throws IllegalStateException    if the patientsList is currently empty
+   * @throws IllegalArgumentException if no PatientRecord with the given caseID is found
+   */
+  public void seePatient(int caseID) throws IllegalStateException, IllegalArgumentException {
+    if (size == 0) {
+      throw new IllegalStateException("The patients list is empty");
+    }
+    //Variable to check if the patient exists
+    boolean exists = false;
+    //Loop through patients List
+    for (int i = 0; i < size; i++) {
+      //Find the patient
+      if (patientsList[i].CASE_NUMBER == caseID) {
+        //Mark the patient as seen
+        patientsList[i].seePatient();
+        exists = true;
+        break;
+      }
+    }
+
+    if (!exists) {
+      throw new IllegalArgumentException("No patient record with this case ID");
+    }
+
+  }
+
+  /**
+   * Creates a formatted String summary of the current state of the patientsList, incorporating an
+   * additional component from the PatientRecord class. The first line displays the current size of
+   * the array. The next displays the current number of patients who have been seen already,
+   * followed by the number of patients at each triage level. Any of these numbers may be 0.
+   *
+   * @return a String summarizing the patientsList
+   */
+  public String getSummary() {
+    int numSeen = 0;
     int numRed = 0;
     int numYellow = 0;
     int numGreen = 0;
-
     for (int i = 0; i < size; i++) {
-      if (patientsList[i][2] == RED) {
+      if (patientsList[i].hasBeenSeen()) {
+        numSeen++;
+      }
+
+      if (patientsList[i].getTriage() == PatientRecord.RED) {
         numRed++;
-      } else if (patientsList[i][2] == YELLOW) {
+      } else if (patientsList[i].getTriage() == PatientRecord.YELLOW) {
         numYellow++;
       } else {
         numGreen++;
       }
     }
 
-    return "Total number of patients: " + size + "\nRED: " + numRed + "\nYELLOW: " + numYellow +
-        "\nGREEN: " + numGreen;
+    //Create the summary message
+    String summary = String.format(
+        "Total number of patients: %d\n" + "Total number seen: %d\n" + "RED: %d\n" +
+            "YELLOW: %d\n" + "GREEN: %d", size, numSeen, numRed, numYellow, numGreen);
+    return summary;
   }
 
+  /**
+   * This method runs occasionally to record the current state of the patientsList and save any
+   * records for seen patients to a file, while removing them from the patientsList to make more
+   * room.
+   *
+   * Every output file begins with the current summary of the patientsList, followed by the string
+   * representation of each SEEN patient on a single line (PatientRecord's toString() method will be
+   * helpful here). The patient records do not need to be in any particular order. If there are NO
+   * seen patients when this method is called, the file will contain only the patientsList summary
+   * (with "Total number seen: 0" as a part of it). If the provided file cannot be written to, do
+   * not modify the patientsList in any way and just return from the method.
+   *
+   * By the end of this method, all SEEN patients should be recorded in the file and removed from
+   * the patientsList array, and the array's size should be updated accordingly.
+   *
+   * For an example of how this method works, please refer to page 6 of the write-up.
+   *
+   * @param file the file object to use for recording the data
+   */
+  public void cleanPatientsList(File file) {
+    //Create a new patients list to add the records that are not seen
+    PatientRecord[] newList = new PatientRecord[patientsList.length];
+    int newIndex = 0;
+    int newSize = 0;
+    try {
+      //Set up a file and write the summary message to it
+      PrintWriter writer = new PrintWriter(file);
+      writer.println(getSummary());
+      //Loop through the patients list
+      for (int i = 0; i < size; i++) {
+        //If patient is seen then add its details to the file
+        if (patientsList[i].hasBeenSeen()) {
+          writer.println(patientsList[i].toString());
+          //If patient is not seen then add it to our new list
+        } else {
+          newList[newIndex] = patientsList[i];
+          newSize++;
+          newIndex++;
+        }
+      }
+      //Equate new list and size to original one
+      size = newSize;
+      patientsList = newList;
+      writer.flush();
+      writer.close();
+    } catch (IOException e) {
+      return;
+    }
+  }
 
+  /**
+   * For testing purposes: this method creates and returns a string representation of the
+   * patientsList, as the in-order string representation of each patient in the list on a
+   * separate line. If patientsList is empty, returns an empty string.
+   *
+   * @return a string representation of the contents of patientsList
+   */
+
+  @Override
+  public String toString() {
+    String returnValue = "";
+    for (PatientRecord r : patientsList) {
+      returnValue += (r != null) ? r.toString() + "\n" : "";
+    }
+    return returnValue.trim();
+  }
 }
